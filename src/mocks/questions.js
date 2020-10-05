788236c7acbf2d49ed24c221fa0ab6d0d51ff6f3
Elizaveta Ozerova <e.ozerova@adsterra.com>
2020-10-05 22:29:15 +0300

    module3-task1 созданы моки

---
+++ src/mocks/questions.js
@@ -0,0 +1,38 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/3`;
+
+export default [
+  {
+    type: `genre`,
+    genre: `classical music`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `techno`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `classical music`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `punk`,
+    }]
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Muse`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      artist: `Maroon 5`,
+      avatar: `${AVATAR_URL}/${Math.random()}`,
+    }, {
+      artist: `Royal Blood`,
+      avatar: `${AVATAR_URL}/${Math.random()}`,
+    }, {
+      artist: `Berliner Philharmoniker`,
+      avatar: `${AVATAR_URL}/${Math.random()}`,
+    }]
+  },
+];
+
