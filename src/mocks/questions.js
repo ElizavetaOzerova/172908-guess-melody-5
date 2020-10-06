2c14202a67b62ed529b29eb02972fbc1586238c0
Elizaveta Ozerova <e.ozerova@adsterra.com>
2020-10-06 13:13:02 +0300

    module3-task1 рефакторинг компонента GenreGameScreen, обработаны пропсы

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -3,10 +3,10 @@ const AVATAR_URL = `https://api.adorable.io/avatars/3`;
 export default [
   {
     type: `genre`,
-    genre: `classical music`,
+    genre: `indi-rock`,
     answers: [{
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
-      genre: `rock`,
+      genre: `indi-rock`,
     }, {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `techno`,
